7e45334c8cacb32a1259b789f8cfbd1bf0862279
Slawa <slawapocht4@yandex.ru>
2021-05-18 21:25:54 +0400

    first day accordion home work

--- js/accordion.js
+++ js/accordion.js
@@ -1,15 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const featureLinkElems = document.querySelectorAll('.feature__link');
 	const featureSubElems = document.querySelectorAll('.feature-sub');
-	featureLinkElems[0].classList.add('feature__link_active');
-	featureSubElems[0].classList.remove('hidden');
+	// featureLinkElems[0].classList.add('feature__link_active');
+	// featureSubElems[0].classList.remove('hidden');
 
 	featureLinkElems.forEach((btn, i) => {
 		btn.addEventListener('click', () => {
-			featureSubElems.forEach((element) => element.classList.add('hidden'));
-			featureLinkElems.forEach((element) => element.classList.remove('feature__link_active'));
-			featureSubElems[i].classList.remove('hidden');
-			featureLinkElems[i].classList.add('feature__link_active');
+			if (btn.classList.contains('feature__link_active')) {
+				featureSubElems[i].classList.add('hidden');
+				featureLinkElems[i].classList.remove('feature__link_active');
+			}else {
+				featureSubElems.forEach((element) => element.classList.add('hidden'));
+				featureLinkElems.forEach((element) => element.classList.remove('feature__link_active'));
+				featureSubElems[i].classList.remove('hidden');
+				featureLinkElems[i].classList.add('feature__link_active');
+			}
 		});
 	});
 });
